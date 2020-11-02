e6adc871076ae6deba4af10536f3ca9345a57948
Tanvy <dggaltanvir@gmail.com>
2020-11-01 22:48:09 -0500

    Model ready
    A LSTM model with 3 layers of LSTM, Dense and Dropouts.

--- Projects/TextClassification/index.py
+++ Projects/TextClassification/index.py
@@ -18,6 +18,7 @@ import pickle
 
 import sklearn 
 from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
 
 import keras
 from keras.preprocessing.sequence import pad_sequences
@@ -85,17 +86,22 @@ y = []
 for i in range(0, 100000, 10000):
     x.append(X[i:i+10000])
     y.append(Y[i:i+10000])    
+ 
+
+def processData(X, Y):
+    b = np.zeros([len(X),maxLen])
+    for i,j in enumerate(X):
+        b[i][0:len(j)] = j
     
+    X = np.reshape(b, (b.shape[0], b.shape[1], 1))
+    Y = np.reshape(Y, (b.shape[0], 1))
+    return X, Y
+    
+X, Y = processData(x[0], y[0])
 
-b = np.zeros([len(X),maxLen])
-for i,j in enumerate(X):
-    b[i][0:len(j)] = j
-
-X = np.reshape(b, (b.shape[0], b.shape[1], 1))
-Y = np.reshape(Y, (b.shape[0], 1))
-
-classifier = Model(X, Y)
+classifier = Model()
 classifier.createModel()
-classifier.fitModel(epochs=100, batchSize=32)
-
+classifier.fitModel(epochs=100, batchSize=32, X=X, Y=Y)
 model = classifier.classifier
+
+model.save("NLP_classifier.mdl")
